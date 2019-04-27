1f46220b25396fb14f35899f56f842cadbdc2f4f
Jiawei Zhuang <jiaweizhuang@g.harvard.edu>
2019-04-27 16:19:27 -0400

    animation for drug field yz cross section

--- JiaweiZhuang/make_movie.sh
+++ JiaweiZhuang/make_movie.sh
@@ -7,6 +7,11 @@ mkdir -p movie
 # If file names are not continuous
 # https://video.stackexchange.com/questions/7300/how-to-get-ffmpeg-to-join-non-sequential-image-files-skip-by-3s
 
+if false; then
+
+echo 'making movie for blood field'
+sleep 1
+
 # Re=5 case
 ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re5pe1/density/density_*.png' -pix_fmt yuv420p ./movie/density_yzcross_re5pe1.mp4
 ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re5pe1/uz/uz_*.png' -pix_fmt yuv420p ./movie/uz_yzcross_re5pe1.mp4
@@ -14,3 +19,24 @@ ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re5pe1/uz/uz_*.png' -
 # Re=10case
 ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re10pe1/density/density_*.png' -pix_fmt yuv420p ./movie/density_yzcross_re10pe1.mp4
 ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re10pe1/uz/uz_*.png' -pix_fmt yuv420p ./movie/uz_yzcross_re10pe1.mp4
+
+fi
+
+
+if true; then
+
+    echo 'making movie for blood field'
+    sleep 1
+
+# Re=5 Pe=1 case
+ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re5pe1/drug/large_vrange/drug_*.png' -pix_fmt yuv420p ./movie/drug_yzcross_largev_re5pe1.mp4
+ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re5pe1/drug/small_vrange/drug_*.png' -pix_fmt yuv420p ./movie/drug_yzcross_smallv_re5pe1.mp4
+
+# Re=5 Pe=3 case
+ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re5pe3/drug/large_vrange/drug_*.png' -pix_fmt yuv420p ./movie/drug_yzcross_largev_re5pe3.mp4
+ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re5pe3/drug/small_vrange/drug_*.png' -pix_fmt yuv420p ./movie/drug_yzcross_smallv_re5pe3.mp4
+
+# Re=10 Pe=1 case
+ffmpeg -framerate 24 -pattern_type glob -i './img/yz_cross/re10pe1/drug/large_vrange/drug_*.png' -pix_fmt yuv420p ./movie/drug_yzcross_largev_re10pe1.mp4
+
+fi
